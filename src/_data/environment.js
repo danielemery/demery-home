a6ec629d5d980d4ad8fc4890b11eb79b9d9807eb
Daniel Emery <danielremery@gmail.com>
2023-10-19 19:59:11 +0200

    #4 Add site version footer

--- src/_data/environment.js
+++ src/_data/environment.js
@@ -1,5 +1,7 @@
 module.exports = function () {
   return {
     email: process.env.EMAIL || "",
+    gitFullCommitHash: process.env.GIT_COMMIT_HASH || "local_development",
+    gitShortCommitHash: process.env.GIT_COMMIT_HASH || "local_development",
   };
 };
