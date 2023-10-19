6d2a1684904ba32c7bc900c8900c8e64a18537d3
Daniel Emery <danielremery@gmail.com>
2023-10-19 20:30:39 +0200

    include git commit of current version in website (#8)
    * Move links into a constants file
    * #4 Add site version footer
    * #4 Set version info and make available to build jobs

---
+++ src/_data/constants.js
@@ -0,0 +1,9 @@
+module.exports = function () {
+  return {
+    githubAccountLink: "https://github.com/danielemery",
+    websiteRepositoryName: "demery-home",
+    blogLink: "https://invariablyabandoned.com",
+    cvLink: "https://cv.demery.net",
+    linkedInLink: "https://www.linkedin.com/in/danielemery-au/",
+  };
+};
--- src/_data/environment.js
+++ src/_data/environment.js
@@ -1,5 +1,9 @@
 module.exports = function () {
   return {
     email: process.env.EMAIL || "",
+    versionName: process.env.SITE_VERSION_NAME || "local_development",
+    versionLink:
+      process.env.SITE_VERSION_LINK ||
+      "https://github.com/danielemery/demery-home",
   };
 };
